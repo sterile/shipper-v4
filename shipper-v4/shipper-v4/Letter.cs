3868abe4e15e3f913a9b76f1de95793006e3f288
Brandon Berney <brandon.berney@louisville.edu>
2020-09-30 09:59:42 -0400

    letter: add it to the shipper_v4 namespace

--- shipper-v4/shipper-v4/Letter.cs
+++ shipper-v4/shipper-v4/Letter.cs
@@ -13,54 +13,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-public class Letter : Parcel
+namespace shipper_v4
 {
-    private decimal _fixedCost; // Cost to send letter
-
-    // Precondition:  cost >= 0
-    // Postcondition: The letter is created with the specified values for
-    //                origin address, destination address, and cost
-    public Letter(Address originAddress, Address destAddress, decimal cost)
-        : base(originAddress, destAddress)
+    public class Letter : Parcel
     {
-        FixedCost = cost;
-    }
+        private decimal _fixedCost; // Cost to send letter
 
-    private decimal FixedCost // Helper property
-    {
+        // Precondition:  cost >= 0
+        // Postcondition: The letter is created with the specified values for
+        //                origin address, destination address, and cost
+        public Letter(Address originAddress, Address destAddress, decimal cost)
+            : base(originAddress, destAddress)
+        {
+            FixedCost = cost;
+        }
+
+        private decimal FixedCost // Helper property
+        {
+            // Precondition:  None
+            // Postcondition: The letter's fixed cost has been returned
+            get
+            {
+                return _fixedCost;
+            }
+
+            // Precondition:  value >= 0
+            // Postcondition: The letter's fixed cost has been set to the
+            //                specified value
+            set
+            {
+                if (value >= 0)
+                    _fixedCost = value;
+                else
+                    throw new ArgumentOutOfRangeException($"{nameof(FixedCost)}", value,
+                        $"{nameof(FixedCost)} must be >= 0");
+            }
+        }
+
         // Precondition:  None
-        // Postcondition: The letter's fixed cost has been returned
-        get
+        // Postcondition: The letter's cost has been returned
+        public override decimal CalcCost()
         {
-            return _fixedCost;
+            return FixedCost;
         }
 
-        // Precondition:  value >= 0
-        // Postcondition: The letter's fixed cost has been set to the
-        //                specified value
-        set
+        // Precondition:  None
+        // Postcondition: A String with the letter's data has been returned
+        public override string ToString()
         {
-            if (value >= 0)
-                _fixedCost = value;
-            else
-                throw new ArgumentOutOfRangeException($"{nameof(FixedCost)}", value,
-                    $"{nameof(FixedCost)} must be >= 0");
+            string NL = Environment.NewLine; // NewLine shortcut
+
+            return $"Letter{NL}{base.ToString()}"; // Let base class help
         }
-    }
-
-    // Precondition:  None
-    // Postcondition: The letter's cost has been returned
-    public override decimal CalcCost()
-    {
-        return FixedCost;
-    }
-
-    // Precondition:  None
-    // Postcondition: A String with the letter's data has been returned
-    public override string ToString()
-    {
-        string NL = Environment.NewLine; // NewLine shortcut
-
-        return $"Letter{NL}{base.ToString()}"; // Let base class help
     }
 }
+
